705db3eab977b4a849d78bfca9d8b68926bb2d1d
Sosthenes Boame <kwame.boame@live.com>
2024-10-21 09:27:34 +0000

    removed name from accounts signal

--- accounts/models.py
+++ accounts/models.py
@@ -77,5 +77,5 @@ class UserProfile(models.Model):
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
-        UserProfile.objects.create(user=instance, name=instance.username)
+        UserProfile.objects.create(user=instance)
     instance.profile.save()
