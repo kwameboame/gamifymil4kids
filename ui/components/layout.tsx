111d1917356a6a00bc699c3fa6f2040e4b1798df
Sosthenes Kwame Boame <kwame.boame@live.com>
2025-06-05 13:16:49 +0000

    logos

--- ui/components/layout.tsx
+++ ui/components/layout.tsx
@@ -29,18 +29,18 @@ export default function Layout({ children }: LayoutProps) {
                   <li><Link href="#" className="hover:text-gray-300">Terms of Service</Link></li>
                 </ul>
               </div>
-              <div className="w-full md:w-1/3">
-                <h4 className="text-md font-semibold mb-2">Project Partners</h4>
+              <div className="w-full md:w-1/3 p-6 bg-white">
+                <h4 className="text-md font-semibold mb-2 text-black">Project Partners</h4>
                 <div className="flex space-x-4">
                   {/* Replace with actual sponsor logos */}
-                  <Image src="/images/ppblogo.png" alt="PenPlusBytes" width={50} height={50} />
-                  {/* <Image src="/images/sponsor2.png" alt="Sponsor 2" width={50} height={50} /> */}
+                  <Image src="/images/ppblogo.png" alt="PenPlusBytes" width={100} />
+                  <Image src="/images/aosf.jpg" alt="AOSF" width={100} />
                   {/* <Image src="/images/sponsor3.png" alt="Sponsor 3" width={50} height={50} /> */}
                 </div>
               </div>
             </div>
             <div className="mt-8 text-center text-sm">
-              <p>&copy; 2024 StorylineGame. All rights reserved.</p>
+              <p>&copy; {new Date().getFullYear()} GamifyMil. All rights reserved.</p>
             </div>
           </div>
         </footer>
